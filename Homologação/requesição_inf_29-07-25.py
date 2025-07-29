4e472cd70c405296b03c34c0b641ea3b0a7b2198
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2025-07-29 06:52:27 -0300

    Update requesição_inf_29-07-25.py
    add_p

--- Homologação/requesição_inf_29-07-25.py
+++ Homologação/requesição_inf_29-07-25.py
@@ -12,11 +12,11 @@ for i in query_acesso:
         tag_busca=refinamento.find(',')
         exec_.append(0)
         try:
-            #verifica se contem complemento
             print(f'{len(exec_)}ª execução')
             refinamento_0=str(refinamento[tag_busca+1:]).replace(' ','')
             refinamento_0=re.sub(r'[^a-zA-Z0-9]','',str(refinamento_0[tag_busca+1:]))
-
+            
+            #verifica se contem complemento
             v=1 if len(refinamento_0)>0 else 0
             if len(exec_)==1 and v==1 and endereco['emitente']['complemento']=='':
                 print('alocação emitente') 
@@ -28,13 +28,15 @@ for i in query_acesso:
                 print(f'resultado:{refinamento_0}')
         except:
             pass
-        if len(exec_)==2:
-            endereco['destinatario']['logradouro']=str(refinamento[:tag_busca])
-            endereco['destinatario']['numero']=re.sub(r'\D','',refinamento[tag_busca:])
-        if len(exec_)==1:
-            endereco['emitente']['logradouro']=str(refinamento[:tag_busca])
-            endereco['emitente']['numero']=re.sub(r'\D','',refinamento[tag_busca:])
-        
+        try:
+            if len(exec_)==2 and endereco['destinatario']['logradouro']=='':
+                endereco['destinatario']['logradouro']=str(refinamento[:tag_busca])
+                endereco['destinatario']['numero']=re.sub(r'\D','',refinamento[tag_busca:])
+            if len(exec_)==1 and endereco['emitente']['logradouro']=='':
+                endereco['emitente']['logradouro']=str(refinamento[:tag_busca])
+                endereco['emitente']['numero']=re.sub(r'\D','',refinamento[tag_busca:])
+        except Exceptions as e:
+            print(f'Erro ao retornar endereços {e}')
         
         refinamento=refinamento.replace(',','')
                
